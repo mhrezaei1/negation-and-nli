0f825e32e489cc02e2ae87a8ff215f3ddbfd22bc
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-06-07 13:06:04 -0700

    try

---
+++ m.sh
@@ -0,0 +1,10 @@
+inputs=(
+    "roberta-large-nsp-1000000-1e-06-32"
+    "roberta-large-pp-500000-1e-06-128"
+    "roberta-large-dual-500000-1e-06-128"
+    "roberta-base-pp-1000000-1e-06-128"
+    "roberta-base-nsp-1000000-1e-06-32"
+    "roberta-base-dual-1000000-1e-06-128"
+)
+
+for input in "${inputs[@]}" do python3 con.py --path "$input" done
