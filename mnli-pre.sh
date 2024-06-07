45da249e301cffbf649d57e04a06ce2aaa5acfe2
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-06-07 09:00:09 -0700

    try

--- mnli-pre.sh
+++ mnli-pre.sh
@@ -13,11 +13,27 @@ inputs=(
 # Maximum number of concurrent jobs, equals to the number of GPUs
 MAX_JOBS=4
 
+# Directory to store the downloaded models
+MODEL_DIR=./downloaded_models
+
 # Function to check and wait for available job slot
 check_jobs() {
   while [ $(jobs -p | wc -l) -ge $MAX_JOBS ]; do
     sleep 2
   done
+}
+
+# Function to download model from Hugging Face if not already downloaded
+download_model() {
+  local model_name=$1
+  local model_path="$MODEL_DIR/$model_name"
+  if [ ! -d "$model_path" ]; then
+    echo "Downloading model: $model_name"
+    mkdir -p "$model_path"
+    python -c "from transformers import AutoModel; AutoModel.from_pretrained('$model_name').save_pretrained('$model_path')"
+  else
+    echo "Model $model_name already downloaded."
+  fi
 }
 
 # Loop through inputs and execute them
@@ -31,6 +47,9 @@ for i in "${!inputs[@]}"; do
     # Split input into variables
     roberta_type=$(echo ${inputs[$i]} | awk '{print $1}')
     roberta_path=$(echo ${inputs[$i]} | awk '{print $2}')
+
+    # Download the model locally
+    download_model "mhr2004/$roberta_path"
 
     echo "Starting job $i on GPU $gpu_index"
 
@@ -52,7 +71,7 @@ for i in "${!inputs[@]}"; do
     export PRED_DIR=./outputs/predictions/MNLI/RoBERTa/$RoBERTa_Path/original_dev
 
     # Execute the training script with CUDA_VISIBLE_DEVICES set for the specific GPU
-    CUDA_VISIBLE_DEVICES=$gpu_index python ./transformers/examples/run_glue.py --model_type roberta --model_name_or_path mhr2004/$roberta_path --task_name $TASK_NAME --do_train --do_eval --do_lower_case --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_gpu_train_batch_size 32 --save_steps 20000 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir ./outputs/models/$TASK_NAME/RoBERTa/$RoBERTa_Path &
+    CUDA_VISIBLE_DEVICES=$gpu_index python ./transformers/examples/run_glue.py --model_type roberta --model_name_or_path $MODEL_DIR/mhr2004/$roberta_path --task_name $TASK_NAME --do_train --do_eval --do_lower_case --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_gpu_train_batch_size 32 --save_steps 20000 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir ./outputs/models/$TASK_NAME/RoBERTa/$RoBERTa_Path &
 
 done
 
@@ -61,4 +80,4 @@ wait
 
 echo "All processes completed"
 
-curl -d "MNLI trainings finished" ntfy.sh/mhrnlpmodels 
+curl -d "MNLI trainings finished" ntfy.sh/mhrnlpmodels
