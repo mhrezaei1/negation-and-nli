27d48690ea9206de62943be89ac916f8308ca9e3
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-06-07 08:49:27 -0700

    try

--- mnli-evaluate.sh
+++ mnli-evaluate.sh
@@ -11,8 +11,6 @@ cp ./data/new_benchmarks/processed_for_run/MNLI/dev_mismatched.tsv ./data/GLUE/M
 
 # create directories that contain predicted labels (as well as actual labels)
 mkdir outputs/predictions/MNLI/RoBERTa/new_dev
-mkdir outputs/predictions/MNLI/XLNet/new_dev
-mkdir outputs/predictions/MNLI/BERT/new_dev
 
 # export task directory and name
 export GLUE_DIR=./data/GLUE/
@@ -25,16 +23,4 @@ export TASK_NAME=MNLI
 # Evaluate on the new pairs containing negation
 export PRED_DIR=./outputs/predictions/MNLI/RoBERTa/new_dev/
 python ./transformers/examples/run_glue.py --model_type roberta --model_name_or_path roberta-base --task_name $TASK_NAME --do_eval --do_lower_case --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_gpu_train_batch_size 32 --save_steps 20000 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir ./outputs/models/$TASK_NAME/RoBERTa/
-
-
-# Using XLNet
-# Evaluate on the new pairs containing negation
-export PRED_DIR=./outputs/predictions/MNLI/XLNet/new_dev/
-python ./transformers/examples/run_glue.py --model_type xlnet --model_name_or_path xlnet-base-cased --task_name $TASK_NAME --do_eval --do_lower_case --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_gpu_train_batch_size 32 --save_steps 20000 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir ./outputs/models/$TASK_NAME/XLNet/
-
-
-# Using BERT
-# Evaluate on the new pairs containing negation
-export PRED_DIR=./outputs/predictions/MNLI/BERT/new_dev/
-python ./transformers/examples/run_glue.py --model_type bert --model_name_or_path bert-base-cased --task_name $TASK_NAME --do_eval --do_lower_case --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_gpu_train_batch_size 32 --save_steps 20000 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir ./outputs/models/$TASK_NAME/BERT/
 
--- mnli-pre.sh
+++ mnli-pre.sh
@@ -1,24 +1,64 @@
-#!/bin/sh
+#!/bin/bash
 
-export RoBERTa_Type = "roberta-base"
-export RoBERTa_Path = "roberta-base"
+# Define your inputs here. For example:
+inputs=(
+    "roberta-base roberta-base-dual-1000000-1e-06-128"
+    "roberta-base roberta-base-pp-500000-1e-06-128"
+    "roberta-base roberta-base-nsp-1000000-1e-06-32"
+    "roberta-large roberta-large-pp-500000-1e-06-128"
+    "roberta-large roberta-large-nsp-1000000-1e-06-32-pp-1000000-1e-06-128"
+    "roberta-large roberta-large-dual-500000-1e-06-128"
+)
 
-# create directories that contain predicted labels (as well as actual labels)
-mkdir outputs/predictions/MNLI
-mkdir outputs/predictions/MNLI/RoBERTa/$RoBERTa_Path
-mkdir outputs/predictions/MNLI/RoBERTa/$RoBERTa_Path/original_dev
+# Maximum number of concurrent jobs, equals to the number of GPUs
+MAX_JOBS=4
 
-# create directories that contain fine-tuned models
-mkdir outputs/models/MNLI
-mkdir outputs/models/MNLI/RoBERTa/$RoBERTa_Path
+# Function to check and wait for available job slot
+check_jobs() {
+  while [ $(jobs -p | wc -l) -ge $MAX_JOBS ]; do
+    sleep 2
+  done
+}
 
-export GLUE_DIR=./data/GLUE/
-export TASK_NAME=MNLI
+# Loop through inputs and execute them
+for i in "${!inputs[@]}"; do
+    # Check if we need to wait for a job slot to become available
+    check_jobs
+    
+    # Calculate GPU index: i % MAX_JOBS ensures cycling through GPUs 0 to MAX_JOBS-1
+    gpu_index=$((i % MAX_JOBS))
 
-# Using RoBERTa 
-#Train on the original training split and evaluate on the original dev split:  
-# export PRED_DIR=./outputs/predictions/MNLI/RoBERTa/original_dev/
-export PRED_DIR=./outputs/predictions/MNLI/RoBERTa/$RoBERTa_Path/original_dev
+    # Split input into variables
+    roberta_type=$(echo ${inputs[$i]} | awk '{print $1}')
+    roberta_path=$(echo ${inputs[$i]} | awk '{print $2}')
 
-# python ./transformers/examples/run_glue.py --model_type roberta --model_name_or_path roberta-base --task_name $TASK_NAME --do_train --do_eval --do_lower_case --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_gpu_train_batch_size 32 --save_steps 20000 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir ./outputs/models/$TASK_NAME/RoBERTa/
-python ./transformers/examples/run_glue.py --model_type roberta --model_name_or_path $RoBERTa_Path --task_name $TASK_NAME --do_train --do_eval --do_lower_case --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_gpu_train_batch_size 32 --save_steps 20000 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir ./outputs/models/$TASK_NAME/RoBERTa/$RoBERTa_Path
+    echo "Starting job $i on GPU $gpu_index"
+
+    # Set environment variables
+    export RoBERTa_Type=$roberta_type
+    export RoBERTa_Path=$roberta_path
+
+    # Create directories that contain predicted labels (as well as actual labels)
+    mkdir -p outputs/predictions/MNLI/RoBERTa/$RoBERTa_Path/original_dev
+
+    # Create directories that contain fine-tuned models
+    mkdir -p outputs/models/MNLI/RoBERTa/$RoBERTa_Path
+
+    export GLUE_DIR=./data/GLUE/
+    export TASK_NAME=MNLI
+
+    # Using RoBERTa 
+    # Train on the original training split and evaluate on the original dev split:
+    export PRED_DIR=./outputs/predictions/MNLI/RoBERTa/$RoBERTa_Path/original_dev
+
+    # Execute the training script with CUDA_VISIBLE_DEVICES set for the specific GPU
+    CUDA_VISIBLE_DEVICES=$gpu_index python ./transformers/examples/run_glue.py --model_type roberta --model_name_or_path mhr2004/$roberta_path --task_name $TASK_NAME --do_train --do_eval --do_lower_case --data_dir $GLUE_DIR/$TASK_NAME --max_seq_length 128 --per_gpu_train_batch_size 32 --save_steps 20000 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir ./outputs/models/$TASK_NAME/RoBERTa/$RoBERTa_Path &
+
+done
+
+# Wait for all background jobs to finish
+wait
+
+echo "All processes completed"
+
+curl -d "MNLI trainings finished" ntfy.sh/mhrnlpmodels 
