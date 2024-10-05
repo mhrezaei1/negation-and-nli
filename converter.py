730ca8c29bac0071153984320d74049495d3231e
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-10-05 15:45:37 -0700

    try

--- converter.py
+++ converter.py
@@ -1,19 +1,35 @@
-from argparse import ArgumentParser
-args = ArgumentParser()
-args.add_argument('--path', type=str, required=True)
-args = args.parse_args()
+# from argparse import ArgumentParser
 
+# args = ArgumentParser()
+# args.add_argument('--path', type=str, required=True)
+# args = args.parse_args()
 from transformers import AutoModel, AutoTokenizer
 
-AutoModel.from_pretrained('mhr2004/' + args.path).save_pretrained('./negation-and-nli/downloaded_models/mhr2004/' + args.path)
-AutoTokenizer.from_pretrained('mhr2004/' + args.path).save_pretrained('./negation-and-nli/downloaded_models/mhr2004/' + args.path)
+model_paths = [
+"bert-large-uncased-nsp-1000000-1e-06-64",
+"bert-large-uncased-pp-1000000-1e-06-32",
+"bert-large-uncased-dual-1000000-1e-06-32",
+"bert-base-uncased-pp-1000000-1e-06-32",
+"bert-base-uncased-nsp-1000000-1e-06-32",
+"bert-base-uncased-dual-1000000-1e-06-32",
+]
 
-from safetensors.torch import load_file
-import torch
-# Load the safetensors model
-state_dict = load_file('./negation-and-nli/downloaded_models/mhr2004/' + args.path + '/model.safetensors')
+class Args:
+    def __init__(self, path):
+        self.path = path
 
-# Save as PyTorch model
-pytorch_model_path = './negation-and-nli/downloaded_models/mhr2004/roberta-large-dual-500000-1e-06-128/pytorch_model.bin'
-pytorch_model_path = './negation-and-nli/downloaded_models/mhr2004/' + args.path + '/pytorch_model.bin'
-torch.save(state_dict, pytorch_model_path)
+for path in model_paths:
+    args = Args(path)
+    AutoModel.from_pretrained('mhr2004/' + args.path).save_pretrained('./negation-and-nli/downloaded_models/mhr2004/' + args.path)
+    AutoTokenizer.from_pretrained('mhr2004/' + args.path).save_pretrained('./negation-and-nli/downloaded_models/mhr2004/' + args.path)
+
+    import torch
+    from safetensors.torch import load_file
+
+    # Load the safetensors model
+    state_dict = load_file('./negation-and-nli/downloaded_models/mhr2004/' + args.path + '/model.safetensors')
+
+    # Save as PyTorch model
+    pytorch_model_path = './negation-and-nli/downloaded_models/mhr2004/roberta-large-dual-500000-1e-06-128/pytorch_model.bin'
+    pytorch_model_path = './negation-and-nli/downloaded_models/mhr2004/' + args.path + '/pytorch_model.bin'
+    torch.save(state_dict, pytorch_model_path)
