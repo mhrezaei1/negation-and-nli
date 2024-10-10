d198745185b97f9a67897a93e2658f00259b48ac
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-10-09 18:43:04 -0700

    try

--- transformers/transformers/tokenization_utils.py
+++ transformers/transformers/tokenization_utils.py
@@ -230,8 +230,7 @@ class PreTrainedTokenizer(object):
                 if key == 'additional_special_tokens':
                     assert isinstance(value, (list, tuple)) and all(isinstance(t, str) or (six.PY2 and isinstance(t, unicode)) for t in value)
                 else:
-                    pass
-                    # assert isinstance(value, str) or (six.PY2 and isinstance(value, unicode))
+                    assert isinstance(value, str) or (six.PY2 and isinstance(value, unicode))
                 setattr(self, key, value)
 
 
